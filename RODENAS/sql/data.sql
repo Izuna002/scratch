be2e66b08a6f2c5fd14a57ef9468ac903c50b6b4
Izuna002 <159679683+Izuna002@users.noreply.github.com>
2024-10-26 17:41:22 +0800

    Add files via upload

---
+++ RODENAS/sql/data.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Customers (
+    CustomerID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR (255) NOT NULL,
+    LastName VARCHAR (255) NOT NULL,
+    PhoneNumber VARCHAR (20) NOT NULL,
+    Email VARCHAR (255),
+    RegistrationDate DATE);
+
+CREATE TABLE Transactions ( TransactionID INT AUTO_INCREMENT PRIMARY KEY,
+ CustomerID INT NOT NULL,
+ TransactionDate DATETIME NOT NULL,
+ ServiceType VARCHAR(255),
+ Amount DECIMAL(10,2) NOT NULL,
+ PaymentMethod VARCHAR(50),
+ FOREIGN KEY (CustomerID) 
+ REFERENCES Customers(CustomerID) );
